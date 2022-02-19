210776b5b670529cb33515c9f36bca7de0844304
Eddie Yen <36756732+rgmmmt4r@users.noreply.github.com>
2022-02-19 20:22:42 +0800

    28_TLE, 35

---
+++ leetcode/28_TLE/script.js
@@ -0,0 +1,8 @@
+var searchInsert = function(nums, target) {
+    num_len = nums.length
+    for(var i = 0;i<num_len;i++){
+        if(target <= nums[i])
+            return i
+    }
+    return num_len
+};
